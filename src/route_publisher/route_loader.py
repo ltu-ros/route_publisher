29dde08c13f2af1867c2f50af0d8dfbebc03f13a
nick-paul <npaul@ltu.edu>
2020-04-14 20:07:58 -0400

    Route loader uses yaml parser to avoid unicode errors

--- src/route_publisher/route_loader.py
+++ src/route_publisher/route_loader.py
@@ -1,20 +1,22 @@
-import json
+import yaml
 from route_publisher.msg import Route, RouteCommand
 
 def loadRoute(filename):
     with open(filename, 'r') as f:
-        return jsonToRoute(json.load(f))
+        return jsonToRoute(yaml.safe_load(f))
 
 def jsonToRoute(data):
-    commands = []
-    for c in data['commands']:
-        commands.append(jsonToRouteCommand(c))
+    print(data)
+    return Route(commands=[
+        jsonToRouteCommand(c) for c in data['commands']
+    ])
 
-    return Route(commands=commands)
 
 def jsonToRouteCommand(data):
+    command = data['command']
     return RouteCommand(
             command=data['command'],
+            name=data.get('name', command),
             x=data.get('x', 0),
             y=data.get('y', 0),
             seconds=data.get('seconds', 0))
